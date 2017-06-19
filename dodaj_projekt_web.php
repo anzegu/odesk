5ca307acefa40c48cd87be8e969e6268568e44ae
Zami99 <Tilen.zami@gmail.com>
2017-06-19 23:01:50 +0200

    as
    as

--- dodaj_projekt_web.php
+++ dodaj_projekt_web.php
@@ -8,10 +8,11 @@ include_once 'header.php';
         <form action="dodaj_projekt.php" method="post">
             <tr><td>Vpišite naslov projekta :</td>
                 <td><input type="text" name="title"  size="26" /> </td>
-            <tr><td>Datum:</td><td><input type="datetime-local" name="end_date" ></td></tr>
-            <tr><td>Opišite projekt: </td><td><input type="text" name="description" size="26"/></td></tr>
+            </tr>
             <tr><td>Okvirna cena: </td><td><input type="text" name="okvirna_cena" size="26"/> </td></tr>
             <tr><td>Deadline cena: </td><td><input type="text" name="deadline_cena" size="26"/></td></tr>
+            <tr><td>Datum:</td><td><input type="datetime-local" name="end_date" ></td></tr>
+            <tr><td>Opišite projekt: </td><td><textarea name="description" cols="32" rows="5"></textarea></td></tr>
                                  <tr><td><input  type="submit" value="Shrani" size="26"></td></tr>
         </form>
     </table>
