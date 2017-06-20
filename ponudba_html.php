fb01a50d4dc0eb402fda5f22afe16e64425d32b7
matic99 <matic.ajnik743@gmail.com>
2017-06-20 15:51:20 +0200

    ponudba
    A

--- ponudba_html.php
+++ ponudba_html.php
@@ -8,15 +8,17 @@ include_once 'header.php';
 $_SESSION['project_id'] = $_GET['pid'];
 ?>
     <body>
+        <table>
         <form action="ponudba.php" method="post">
              
-            Cena: <input type="text" name="cena" /> </br>
-            Opis <input type="text" name="opis" /> </br>
+            <tr><td>Cena:</td> <td><input type="text" name="cena" /> </br></td></tr>
+        <tr><td>Opis:</td><td> <input type="text" name="opis" /> </br></td></tr>
               
-                                 <input  type="submit" value="Shrani" >
-                                 <input type="reset" value="Prekliči">
+                                <tr><td> <input  type="submit" value="Shrani" ></td>
+                              <td>   <input type="reset" value="Prekliči"></td></tr>
              
         </form>
+        </table>
     </body>
     <?php
     include_once 'footer.php';
