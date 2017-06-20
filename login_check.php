c6452e21dac0969c16c12a4275b6994c98e07682
matic99 <matic.ajnik743@gmail.com>
2017-06-20 16:21:42 +0200

    a
    a

--- login_check.php
+++ login_check.php
@@ -27,7 +27,7 @@
         $_SESSION['first_name'] = $user['first_name'];
         $_SESSION['last_name'] = $user['last_name'];
         $_SESSION['email'] = $user["email"];
-        
+        $_SESSION['admin'] = $user['admin'];
         //vse je ok, preusmeritev na index
         header("Location: index.php");
         die();
