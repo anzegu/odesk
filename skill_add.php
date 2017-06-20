f125e62336508c0df4c71b183e9a98e35073e3bb
matic99 <matic.ajnik743@gmail.com>
2017-06-20 19:11:39 +0200

    da
    da

--- skill_add.php
+++ skill_add.php
@@ -3,11 +3,12 @@
 ?>
 
 <h1>Dodajanje veščin</h1>
+<table>
 <form action="skill_insert.php" method="POST">
-    Ime: <input type="text" name="title"  /><br />
-    Opis: <textarea name="description" cols="15" rows="5" placeholder="Vnesi pobrobni opis veščine"></textarea><br />
-    <input type="submit" value="Vnesi" /> 
-    <input type="reset" value="Prekliči" />
+    <tr><td> Ime:</td><td> <input type="text" name="title"  /><br /></td></tr>
+    <tr><td>Opis:</td><td> <textarea name="description" cols="15" rows="5" placeholder="Vnesi pobrobni opis veščine"></textarea><br /></td></tr>
+    <tr><td><input type="submit" value="Vnesi" /> 
+    <input type="reset" value="Prekliči" /></td></tr>
 </form>
 
 
